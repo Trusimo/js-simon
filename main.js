d99fdd048bb3ae2e1287d7cf703b840109da8290
Trusimo <simonetrunfio96@gmail.com>
2022-05-31 16:51:50 +0200

    adds number and timer

--- main.js
+++ main.js
@@ -10,3 +10,23 @@
 // Dopo che sono stati inseriti i 5 numeri,
 // il software dice quanti e quali dei numeri
 // da indovinare sono stati individuati.
+
+// Creo i 5 numeri
+document.getElementById("n1").innerHTML = (Math.floor(Math.random() * 100));
+document.getElementById("n2").innerHTML = (Math.floor(Math.random() * 100));
+document.getElementById("n3").innerHTML = (Math.floor(Math.random() * 100));
+document.getElementById("n4").innerHTML = (Math.floor(Math.random() * 100));
+document.getElementById("n5").innerHTML = (Math.floor(Math.random() * 100));
+
+// creo il timer
+let secondi = 30;
+countdownInterval = setInterval(function () {
+
+    secondi--;
+
+    document.getElementById("timer").innerHTML = secondi;
+
+    if (secondi === 0) {
+        clearInterval(countdownInterval);
+    }
+    }, 1000);
